1a6a80444b6fa102861da790ed8ff628346d988c
Driyaannt <driya.ananta@gmail.com>
2022-08-16 11:13:40 +0700

    JMP A - MOCHAMAD DRIYA ANANTA

---
+++ Tugas Pertemuan 10/intent-score-master/intent-score-master/app/src/main/java/id/putraprima/skorbola/MainActivity.java
@@ -0,0 +1,49 @@
+package id.putraprima.skorbola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_next;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText home = findViewById(R.id.home_team);
+        final EditText away = findViewById(R.id.away_team);
+        //TODO
+        //Fitur Main Activity
+        //1. Validasi Input Home Team
+        //Done
+        //2. Validasi Input Away Team
+        //Done
+        //3. Ganti Logo Home Team
+        //Done
+        //4. Ganti Logo Away Team
+        //Done
+        //5. Next Button Pindah Ke MatchActivity
+        btn_next = findViewById(R.id.btn_team);
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String home_ = home.getText().toString();
+                String away_ = away.getText().toString();
+
+                if(!home_.isEmpty() && !away_.isEmpty()){
+                    Intent intent = new Intent(MainActivity.this, MatchActivity.class);
+                    intent.putExtra("home", home_.toString());
+                    intent.putExtra("away", away_.toString());
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Please fill the blank", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
